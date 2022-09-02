9c8e11e348c2c4f847b09c3e4f629048d530a392
Brayan Gómez <57688651+brayangomez22@users.noreply.github.com>
2022-09-01 21:44:48 -0500

    first commit

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,91 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { UsuarioModel } from "../models/usuario.model";
+
+import { map } from "rxjs/operators";
+
+@Injectable({
+  providedIn: "root",
+})
+export class AuthService {
+  private url = "https://identitytoolkit.googleapis.com/v1/accounts:";
+  private apiKey = "AIzaSyApPUdHF-5vDcgRBfzQAaHhEm9E5tg0pLw";
+
+  userToken: string;
+
+  constructor(private http: HttpClient) {
+    this.leerToken();
+  }
+
+  logout() {
+    localStorage.removeItem("token");
+  }
+
+  login(usuario: UsuarioModel) {
+    const authData = {
+      ...usuario,
+      returnSecureToken: true,
+    };
+
+    return this.http
+      .post(`${this.url}signInWithPassword?key=${this.apiKey}`, authData)
+      .pipe(
+        map((resp) => {
+          this.guardarToken(resp["idToken"]);
+          return resp;
+        })
+      );
+  }
+
+  nuevoUsuario(usuario: UsuarioModel) {
+    const authData = {
+      ...usuario,
+      returnSecureToken: true,
+    };
+
+    return this.http
+      .post(`${this.url}signUp?key=${this.apiKey}`, authData)
+      .pipe(
+        map((resp) => {
+          this.guardarToken(resp["idToken"]);
+          return resp;
+        })
+      );
+  }
+
+  private guardarToken(idToken: string) {
+    this.userToken = idToken;
+    localStorage.setItem("token", idToken);
+
+    let hoy = new Date();
+    hoy.setSeconds(3600);
+
+    localStorage.setItem("expira", hoy.getTime().toString());
+  }
+
+  leerToken() {
+    if (localStorage.getItem("token")) {
+      this.userToken = localStorage.getItem("token");
+    } else {
+      this.userToken = "";
+    }
+
+    return this.userToken;
+  }
+
+  estaAutenticado(): boolean {
+    if (this.userToken.length < 2) {
+      return false;
+    }
+
+    const expira = Number(localStorage.getItem("expira"));
+    const expiraDate = new Date();
+    expiraDate.setTime(expira);
+
+    if (expiraDate > new Date()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
